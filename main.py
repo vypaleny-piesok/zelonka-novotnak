ce1c69055f698634a0c323c99d21992b55de1483
Adamqoo <novotnak@spse-po.sk>
2024-11-25 09:22:02 +0100

    skuska1

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ ress Double Shift to search everywhere for classes, files, tool windows, actions
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}').
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
